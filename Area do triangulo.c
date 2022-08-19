3e8fddea285f64944ad3af1810aa50b433592333
Marcelo Pilati <109703603+marcelopilati@users.noreply.github.com>
2022-08-19 19:19:52 -0300

    first commit

---
+++ Area do triangulo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+
+    float base, altura, area;
+
+    printf("Insira o valor da base: ");
+    scanf("%f",&base);
+    printf("Insira o valor da altura: ");
+    scanf("%f",&altura);
+
+    area = base * altura;
+    printf("A area do triangulo e: %f", area);
+    }
